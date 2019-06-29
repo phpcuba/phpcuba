435348a214834ebaa71c1e4c7a421d04b3225e99
rafageist <cubapush@gmail.com>
2019-06-28 22:28:43 -0500

    update

--- phpcuba.php
+++ phpcuba.php
@@ -45,6 +45,7 @@ function phpcuba_error($message)
   ];
 }
 
+// --------------------------------- OBJECT AND CLASSES ---------------------------- //
 /**
  * Simple function for get public vars of $this
  *
@@ -80,5 +81,98 @@ function get_object_public_vars($object)
 }
 
 more0001:
+
+/**
+ * Complete object/array properties
+ *
+ * @param mixed   $source
+ * @param mixed   $complement
+ * @param integer $level
+ *
+ * @return mixed
+ */
+if (function_exists('cop')) {
+  phpcuba_error("LOADING: Function cop() already exists");
+  goto more0002;
+}
+
+function cop(&$source, $complement, $level = 0)
+{
+  $null = null;
+
+  if (is_null($source)) {
+    return $complement;
+  }
+
+  if (is_null($complement)) {
+    return $source;
+  }
+
+  if (is_scalar($source) && is_scalar($complement)) {
+    return $complement;
+  }
+
+  if (is_scalar($complement) || is_scalar($source)) {
+    return $source;
+  }
+
+  if ($level < 100) { // prevent infinite loop
+    if (is_object($complement)) {
+      $complement = get_object_vars($complement);
+    }
+
+    foreach ($complement as $key => $value) {
+      if (is_object($source)) {
+        if (property_exists($source, $key)) {
+          $source->$key = cop($source->$key, $value, $level + 1);
+        }
+        else {
+          $source->$key = cop($null, $value, $level + 1);
+        }
+      }
+      if (is_array($source)) {
+        if (key_exists($key, $source)) {
+          $source [$key] = cop($source[$key], $value, $level + 1);
+        }
+        else {
+          $source[$key] = cop($null, $value, $level + 1);
+        }
+      }
+    }
+  }
+
+  return $source;
+}
+
+// ------------------------------------- STRINGS ----------------------------- //
+more0002:
+
+/**
+ * Clear string
+ *
+ * @param String $name
+ * @param String $chars
+ * @param bool $direction True for keep, false for delete
+ *
+ * @return String
+ */
+function str_clear($name, $chars, $direction = true)
+{
+  $l = strlen($name);
+  $new_name = '';
+  $chars .= $extra_chars;
+
+  for($i = 0; $i < $l; $i ++)
+  {
+    $ch = $name[$i];
+    if(stripos($chars, $ch) === $direction)
+    {
+      $new_name .= $ch;
+    }
+  }
+
+  return $new_name;
+}
+
 
 // EOF
